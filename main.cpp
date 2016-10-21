4b49886fa820a5300d00763547df344ce64f4654
kirk8182 <kirk8182@vandals.uidaho.edu>
2016-10-21 16:00:34 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 // CS383 HW 3
 #include <iostream>
 
-#include "stack.cpp"
+#include "stack.h"
 
-int main {
+int main() {
 	Stack s;
 	int c, t;
+	
 	std::cout << "Enter number count: ";
 	std::cin >> c;
+
 	while (c --> 0) {
 		std::cout << "Next Number: ";
 		std::cin >> t;
 		s.Push(t);
 	}
+	
 	c = 0;
 	while (!s.IsEmpty()) {
 		c += s.Pop();
 	}
+	
 	std::cout << c << std::endl;
+	std::cin.ignore();
+	std::cin.get();
 }
 

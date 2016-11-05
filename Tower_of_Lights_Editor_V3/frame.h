e0e440e69f07c624a56ddc25bb3bf55e124db703
kirk8182 <kirk8182@vandals.uidaho.edu>
2016-11-05 15:20:16 -0700

    Delete frame.h
    not needed

--- Tower_of_Lights_Editor_V3/frame.h
+++
@@ -1,6 +0,0 @@
-struct frame
-{
-    double startTime;   //when the frame starts in milliseconds
-    double interval;    //the length of the time in milliseconds
-    rgb pixel[row][col];  //the rgb values for the frame
-};

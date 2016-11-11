892217d92ac41be5dc0d433eb5106339ad5c0743
Hunter Barnett <DreamKontrol@users.noreply.github.com>
2016-11-11 14:18:30 -0800

    Fixed names for Tan File "globals"

--- Tower_of_Lights_Editor_V3/TanFile.h
+++ Tower_of_Lights_Editor_V3/TanFile.h
@@ -8,41 +8,32 @@
 
 // Tower Frame
 struct TanFrame {
-    /*QColor pixel[12][20]; // [x][y] ; tower : x=4..7,y=5..14
-    int duration;    // the length of time the frame will last in ms
-    QTime startTime; //when the frame begins in ms*/
     QColor pixel[12][20]; // [x][y] ; tower : x=4..7,y=5..14
     int frame_length; // >= 25ms
-    QTime frame_start; //when the frame begins (ms)
+    int frame_start; // in ms
 };
 
 // TAN File
 class TanFile {
-public:
+private:
     // Variables
-    /*QString fileName;   //the name of the file
-    QString audioName;  //the name of the audio file
-    QColor leftClick;   //the color for left click
-    QColor rightClick;  //the color for right click
-    QColor preset[16];  //the 16 preset colors
-    QLinkedList<TanFrame*>frame;   //the frame(s)
-    int frameCount;     //the number of frames*/
     QString m_filename_tan;                  //TAN file name
     QString m_filename_wav;                  //WAV file name
-    QColor m_filename_curr;                  //the current color selected
-    QColor m_filename_preset[16];            //the 16 preset colors
+    QColor m_color_left;                     //the current color selected
+    QColor m_color_preset[16];               //the 16 preset colors
     QLinkedList<struct TanFrame *> m_frames; //Frame linked list
-    int frame_count; //number of frames in file
+    // m_frame_count = m_frames.size();      // CONSTANT! DO NOT NEED TO STORE
+    QColor m_color_right;                    // not saved in file
 public:
     TanFile(); // New
     QString getFileName();
     QString getAudioName();
     QColor getCurrRGB();
-    QColor * getPresetRGB();    //returns pointer to QColor array
+    QColor getPresetRGB(int index);
 
     void Save();
-    void SaveAs();
     void SaveAs(const QString& p_filename);
 };
 
 #endif
+

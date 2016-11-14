4704e3bcff1b518fc9700be2e63ffbbf638772fd
Hunter Barnett <DreamKontrol@users.noreply.github.com>
2016-11-14 11:46:13 -0800

    Saves frame data to TAN file

--- Tower_of_Lights_Editor_V3/TanFile.h
+++ Tower_of_Lights_Editor_V3/TanFile.h
@@ -9,51 +9,49 @@
 
 // Tower Frame
 struct TanCell {
-	QColor color;
-	QString name;
+    QColor color;
+    QString name;
 };
 
 struct TanFrame {
-	// [x][y] ; tower : x=4..7,y=5..14
-	struct TanCell pixels[TAN_DEFAULT_COLS][TAN_DEFAULT_ROWS];
-	int frame_length; // >= 25ms
-	int frame_start; // in ms
+    // [x][y] ; tower : x=4..7,y=5..14
+    struct TanCell pixels[TAN_DEFAULT_COLS][TAN_DEFAULT_ROWS];
+    int frame_length; // >= 25ms
+    int frame_start; // in ms
 };
 
 // TAN File
 class TanFile {
-
 private:
-
-	QString m_filename_tan;															// TAN file name
-	QString m_filename_wav;															// WAV file name
-	QColor m_color_left;																// the current color selected
-	QColor m_color_right;																// not saved in file; pulled from the 1st preset color
-	QColor m_color_preset[TAN_DEFAULT_COLORPRESETS];		// preset colors
-	// m_frame_count = m_frames.size();									// CONSTANT! DO NOT NEED TO STORE
+    QString m_filename_tan;                          // TAN file name
+    QString m_filename_wav;                          // WAV file name
+    QColor m_color_left;                             // the current color selected
+    QColor m_color_right;                            // not saved in file; pulled from the 1st preset color
+    QColor m_color_preset[TAN_DEFAULT_COLORPRESETS]; // preset colors
+    // m_frame_count = m_frames.size();              // CONSTANT! DO NOT NEED TO STORE
 
 public:
-	TanFile(); // New
+    TanFile(); // New
 
-	QLinkedList<TanFrame> m_frames; //Frame linked list
-	void setFileName(QString);
-	QString getFileName();
+    QLinkedList<TanFrame> m_frames; //Frame linked list
+    void setFileName(QString);
+    QString getFileName();
 
-	void setAudioFile(QString);
-	QString getAudioFile();
+    void setAudioFile(QString);
+    QString getAudioFile();
 
-	void setLeftColor(int,int,int);
-	QColor getLeftColor();
+    void setLeftColor(int,int,int);
+    QColor getLeftColor();
 
-	void setRightColor(int,int,int);
-	QColor getRightColor();
+    void setRightColor(int,int,int);
+    QColor getRightColor();
 
-	void setPresetColor(int []);
-	QColor getPresetColor(int index);
+    void setPresetColor(int []);
+    QColor getPresetColor(int index);
 
-	void Save();
-	void SaveAs();
-	void SaveAs(const QString &p_filename);
+    void Save();
+    void SaveAs();
+    void SaveAs(const QString &p_filename);
 };
 
-#endif	//TANFILE_H
+#endif // TANFILE_H
--- Tower_of_Lights_Editor_V3/TanFile_Save.cpp
+++ Tower_of_Lights_Editor_V3/TanFile_Save.cpp
@@ -9,61 +9,69 @@ void TanFile::Save() {
 
 void TanFile::SaveAs() {
 /*
-		QStringList fileNames;
+        QStringList fileNames;
     QFileDialog dialog();
-		dialog.setFileMode(QFileDialog::AnyFile);
+        dialog.setFileMode(QFileDialog::AnyFile);
     dialog.setNameFilter(tr("*.tan*;;All Files (*)"));
     dialog.setViewMode(QFileDialog::Detail);
     if(dialog.exec()){
         fileNames = dialog.selectedFiles();
     }
-	SaveAs(fileNames[0]);
+    SaveAs(fileNames[0]);
 */
 }
 
 void TanFile::SaveAs(const QString& p_filename) {
-		QFile real_file(p_filename);
+    QFile real_file(p_filename);
 
     if (real_file.open(QIODevice::WriteOnly)) {
-		QTextStream file(&real_file);
+        QTextStream file(&real_file);
 
-		// Version
-		file << "0.4\r\n";
+        // Version
+        file << "0.4\r\n";
 
-		// Audio File
+        // Audio File
         if (m_filename_wav.isEmpty()) {
-			file << "NoAudioFile\r\n";
-		} else {
-			file << m_filename_wav << "\r\n";
-		}
+            file << "NoAudioFile\r\n";
+        } else {
+            file << m_filename_wav << "\r\n";
+        }
 
-		// Current Color
-				file << m_color_left.red() << " ";
-				file << m_color_left.green() << " ";
-				file << m_color_left.blue() << "\r\n";
+        // Current Color
+        file << m_color_left.red() << " ";
+        file << m_color_left.green() << " ";
+        file << m_color_left.blue() << "\r\n";
 
-		// Palette
-		for (int i = 0; i < TAN_DEFAULT_COLORPRESETS-1; i++) {
-						file << m_color_preset[i].red() << " ";
-						file << m_color_preset[i].green() << " ";
-						file << m_color_preset[i].blue() << " ";
-		}
-		file << m_color_preset[TAN_DEFAULT_COLORPRESETS-1].red() << " ";
-		file << m_color_preset[TAN_DEFAULT_COLORPRESETS-1].green() << " ";
-		file << m_color_preset[TAN_DEFAULT_COLORPRESETS-1].blue() << "\r\n";
+        // Palette
+        for (int i = 0; i < TAN_DEFAULT_COLORPRESETS-1; i++) {
+            file << m_color_preset[i].red() << " ";
+            file << m_color_preset[i].green() << " ";
+            file << m_color_preset[i].blue() << " ";
+        }
+        file << m_color_preset[TAN_DEFAULT_COLORPRESETS-1].red() << " ";
+        file << m_color_preset[TAN_DEFAULT_COLORPRESETS-1].green() << " ";
+        file << m_color_preset[TAN_DEFAULT_COLORPRESETS-1].blue() << "\r\n";
 
-		// Stuff
-		file << m_frames.size() << " 10 4\r\n";
+        // Stuff
+        file << m_frames.size() << " 10 4\r\n";
 
-		// Frames
-		for (int i = 0; i < m_frames.size(); i++) {
-			// TODO
-			// frame ms
-			// frame pixels
-		}
+        // Frames
+        for (int i = 0; i < m_frames.size(); i++) {
+            file << m_frames[i].frame_start << "\r\n";
+            for (int x = 0; x < TAN_DEFAULT_COLS; x++) {
+                for (int y = 0; y < TAN_DEFAULT_ROWS-1; y++) {
+                    file << m_frames[i].pixels[x][y].color.red() << " ";
+                    file << m_frames[i].pixels[x][y].color.green() << " ";
+                    file << m_frames[i].pixels[x][y].color.blue() << " ";
+                }
+                file << m_frames[i].pixels[x][TAN_DEFAULT_ROWS-1].color.red() << " ";
+                file << m_frames[i].pixels[x][TAN_DEFAULT_ROWS-1].color.green() << " ";
+                file << m_frames[i].pixels[x][TAN_DEFAULT_ROWS-1].color.blue() << "\r\n";
+            }
+        }
 
-		real_file.close();
-	} else {
-		// TODO error
-	}
+        real_file.close();
+    } else {
+        // TODO error
+    }
 }
